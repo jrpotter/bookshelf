b3ebddc119082473cd3dd4afd8ddf5b6517d99cc
Joshua Potter <jrpotter2112@gmail.com>
2023-05-02 09:14:50 -0600

    Add std4 library to bookshelf.

--- lakefile.lean
+++ lakefile.lean
@@ -6,6 +6,9 @@ package «bookshelf»
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @
     "0107c50abf149a48b5b9ad08a0b2a2093bcb567a"
+require std4 from git
+  "https://github.com/leanprover/std4.git" @
+    "6006307d2ceb8743fea7e00ba0036af8654d0347"
 
 @[default_target]
 lean_lib «Bookshelf» {
