995e0ce3271b0cdc2ad09ece5415ec42d559aff6
Joshua Potter <jrpotter2112@gmail.com>
2023-05-22 12:53:12 -0600

    Add "Elements of Set Theory" to index.html.

--- DocGen4/Output/Index.lean
+++ DocGen4/Output/Index.lean
@@ -27,6 +27,7 @@ def index : BaseHtmlM Html := do templateExtends (baseHtml "Index") <|
         <li>Axler, Sheldon. Linear Algebra Done Right. Undergraduate Texts in Mathematics. Cham: Springer International Publishing, 2015.</li>
         <li>Cormen, Thomas H., Charles E. Leiserson, Ronald L. Rivest, and Clifford Stein. Introduction to Algorithms. 3rd ed. Cambridge, Mass: MIT Press, 2009.</li>
         <li>Enderton, Herbert B. A Mathematical Introduction to Logic. 2nd ed. San Diego: Harcourt/Academic Press, 2001.</li>
+        <li>Enderton, Herbert B. Elements of Set Theory. New York: Academic Press, 1977.</li>
         <li>Gries, David. The Science of Programming. Texts and Monographs in Computer Science. New York: Springer-Verlag, 1981.</li>
         <li>Gustedt, Jens. Modern C. Shelter Island, NY: Manning Publications Co, 2020.</li>
         <li>Ross, Sheldon. A First Course in Probability Theory. 8th ed. Pearson Prentice Hall, n.d.</li>
