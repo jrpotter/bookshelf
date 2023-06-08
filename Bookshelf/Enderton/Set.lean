7559664d56f07bbd1ac5023e6fc1476f62622993
Joshua Potter <jrpotter2112@gmail.com>
2023-06-08 17:57:02 -0600

    Enderton. Ordered pairs.

--- Bookshelf/Enderton/Set.lean
+++ Bookshelf/Enderton/Set.lean
@@ -1,2 +1,3 @@
 import Bookshelf.Enderton.Set.Chapter_1
 import Bookshelf.Enderton.Set.Chapter_2
+import Bookshelf.Enderton.Set.Chapter_3
--- Bookshelf/Enderton/Set/Chapter_3.lean
+++ Bookshelf/Enderton/Set/Chapter_3.lean
@@ -1,4 +1,7 @@
 import Mathlib.Data.Set.Basic
+import Mathlib.SetTheory.ZFC.Basic
+
+import Common.Set.OrderedPair
 
 /-! # Enderton.Chapter_3
 
@@ -6,5 +9,14 @@ Relations and Functions
 -/
 
 namespace Enderton.Set.Chapter_3
+
+/--
+If `x ∈ C` and `y ∈ C`, then `⟨x, y⟩ ∈ 𝒫 𝒫 C`.
+-/
+theorem theorem_3b {C : Set α} (hx : x ∈ C) (hy : y ∈ C)
+  : Set.OrderedPair x y ∈ 𝒫 𝒫 C := by
+  have hxs : {x} ⊆ C := Set.singleton_subset_iff.mpr hx
+  have hxys : {x, y} ⊆ C := Set.mem_mem_imp_pair_subset hx hy
+  exact Set.mem_mem_imp_pair_subset hxs hxys
 
 end Enderton.Set.Chapter_3
