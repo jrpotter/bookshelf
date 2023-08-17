2c1547aee987144e8df1557c90a18499cc63a3b0
Joshua Potter <jrpotter2112@gmail.com>
2023-08-17 12:56:39 -0600

    Enderton (set). Equinumerosity theorems.

--- Bookshelf/Enderton/Set/Chapter_6.lean
+++ Bookshelf/Enderton/Set/Chapter_6.lean
@@ -18,22 +18,22 @@ For any sets `A`, `B`, and `C`,
 -/
 
 theorem theorem_6a_a (A : Set α)
-  : ∃ f, Set.BijOn f A A := by
+  : ∃ F, Set.BijOn F A A := by
   refine ⟨fun x => x, ?_⟩
   unfold Set.BijOn Set.MapsTo Set.InjOn Set.SurjOn
   simp only [imp_self, implies_true, Set.image_id', true_and]
   exact Eq.subset rfl
 
 theorem theorem_6a_b [Nonempty α] (A : Set α) (B : Set β)
-  (f : α → β) (hf : Set.BijOn f A B)
-  : ∃ g, Set.BijOn g B A := by
-  refine ⟨Function.invFunOn f A, ?_⟩
-  exact (Set.bijOn_comm $ Set.BijOn.invOn_invFunOn hf).mpr hf
+  (F : α → β) (hF : Set.BijOn F A B)
+  : ∃ G, Set.BijOn G B A := by
+  refine ⟨Function.invFunOn F A, ?_⟩
+  exact (Set.bijOn_comm $ Set.BijOn.invOn_invFunOn hF).mpr hF
 
 theorem theorem_6a_c (A : Set α) (B : Set β) (C : Set γ)
-  (f : α → β) (hf : Set.BijOn f A B)
-  (g : β → γ) (hg : Set.BijOn g B C)
-  : ∃ h, Set.BijOn h A C := by
-  exact ⟨g ∘ f, Set.BijOn.comp hg hf⟩
+  (F : α → β) (hF : Set.BijOn F A B)
+  (G : β → γ) (hG : Set.BijOn G B C)
+  : ∃ H, Set.BijOn H A C := by
+  exact ⟨G ∘ F, Set.BijOn.comp hG hF⟩
 
 end Enderton.Set.Chapter_6
