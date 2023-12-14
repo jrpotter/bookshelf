9864ffd7a0253c22f2fead1c48b1417b2506ee7b
Joshua Potter <jrpotter2112@gmail.com>
2023-12-14 14:09:09 -0700

    Remove embedded doc-gen4.

--- DocGen4/Output/Instance.lean
+++
@@ -1,8 +0,0 @@
-import DocGen4.Output.Template
-import DocGen4.Output.Definition
-
-namespace DocGen4
-namespace Output
-
-end Output
-end DocGen4
--- static/site-root.js
+++
@@ -1,4 +0,0 @@
-/**
- * Get the site root, {siteRoot} is to be replaced by doc-gen4.
- */
-export const SITE_ROOT = "{siteRoot}";
