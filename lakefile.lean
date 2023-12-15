fbe1e685d3ba6dc75d8095369e0a98d4e77b8f4f
Joshua Potter <jrpotter2112@gmail.com>
2023-12-15 14:45:22 -0700

    Load doc-gen4 as a git dependency.

--- lakefile.lean
+++ lakefile.lean
@@ -10,8 +10,9 @@ require mathlib from git
 require std from git
   "https://github.com/leanprover/std4.git" @
     "v4.3.0"
-meta if get_config? env = some "dev" then
-  require «doc-gen4» from "./doc-gen4"
+require «doc-gen4» from git
+  "https://github.com/jrpotter/bookshelf-doc" @
+    "main"
 
 @[default_target]
 lean_lib «Bookshelf» {
